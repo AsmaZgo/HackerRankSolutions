2e06b2171935dcce614e30d2c58dfd9d09c5dff6
zgolli <zgolliasma@gmail.com>
2023-10-06 23:38:42 +0200

    some refactoring

--- leetcode/twoNumbersAdd/solution.py
+++ leetcode/twoNumbersAdd/solution.py
@@ -1,8 +1,10 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -10,64 +12,64 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        cur=0
-        res=ListNode()
+        # For simplicity reasons we don't verify the constraints
+        cur = 0
+        res = ListNode()
         if l1 == None and l2 == None:
             return None
-        
+
         if l1 <> None:
-            cur=cur+l1.val
+            cur = cur + l1.val
         if l2 <> None:
-            cur=cur+l2.val
-        res.val=cur % 10
-        res.next=ListNode()
-        res.next.val= cur / 10
+            cur = cur + l2.val
+        res.val = cur % 10
+        res.next = ListNode()
+        res.next.val = cur / 10
         if l1 == None:
-            self.addTwoNumbers_p(l1,l2.next,res.next)
-        elif l2== None:
-            self.addTwoNumbers_p(l1.next,l2,res.next)
+            self.addTwoNumbers_p(l1, l2.next, res.next)
+        elif l2 == None:
+            self.addTwoNumbers_p(l1.next, l2, res.next)
         else:
-            if l1.next == None and l2.next == None and res.next.val ==0:
-                res.next=None
+            if l1.next == None and l2.next == None and res.next.val == 0:
+                res.next = None
             else:
-                self.addTwoNumbers_p(l1.next,l2.next,res.next)
+                self.addTwoNumbers_p(l1.next, l2.next, res.next)
         return res
 
-    def addTwoNumbers_p(self, l1, l2,curr):
+    def addTwoNumbers_p(self, l1, l2, curr):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
-        cur=curr.val
-        res=ListNode()
+        cur = curr.val
         if l1 == None and l2 == None:
-            curr=None
-        
+            curr = None
+
             return True
-        
+
         if l1 <> None:
-            cur=cur+l1.val
+            cur = cur + l1.val
         if l2 <> None:
-            cur=cur+l2.val
-        curr.val=cur % 10
-        
-        curr.next=ListNode()
-        curr.next.val= cur / 10
-        
+            cur = cur + l2.val
+        curr.val = cur % 10
+
+        curr.next = ListNode()
+        curr.next.val = cur / 10
+
         if l1 == None:
-            if l2.next == None and curr.next.val ==0:
-                curr.next=None
+            if l2.next == None and curr.next.val == 0:
+                curr.next = None
             else:
-                self.addTwoNumbers_p(l1,l2.next,curr.next)
-        elif l2== None:
-            if l1.next == None and curr.next.val ==0:
-                curr.next=None
+                self.addTwoNumbers_p(l1, l2.next, curr.next)
+        elif l2 == None:
+            if l1.next == None and curr.next.val == 0:
+                curr.next = None
             else:
-                self.addTwoNumbers_p(l1.next,l2,curr.next)
+                self.addTwoNumbers_p(l1.next, l2, curr.next)
         else:
-            if l1.next == None and l2.next == None and curr.next.val ==0:
-                curr.next=None
+            if l1.next == None and l2.next == None and curr.next.val == 0:
+                curr.next = None
             else:
-                self.addTwoNumbers_p(l1.next,l2.next,curr.next)
+                self.addTwoNumbers_p(l1.next, l2.next, curr.next)
         return False
--- twosums/solution.py
+++ twosums/solution.py
@@ -5,14 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        ind_i=0
-        
+        ind_i = 0
         for i in nums:
-            ind_j=0
+            ind_j = 0
             for j in nums:
-                if (ind_i <> ind_j) and (i+j == target):
+                if (ind_i <> ind_j) and (i + j == target):
                     return [ind_i, ind_j]
-                ind_j=ind_j+1
-            
-            ind_i=ind_i+1
+                ind_j = ind_j + 1
+
+            ind_i = ind_i + 1
         return []
